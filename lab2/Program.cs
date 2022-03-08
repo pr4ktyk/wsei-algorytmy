63dea579992082c4ffff134e40edc8461e9c04e4
Łukasz Wąś <ljwasiu@gmail.com>
2022-03-08 20:33:15 +0100

    +zad1

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -6,7 +6,20 @@ namespace lab2
     {
         static void Main(string[] args)
         {
+            int[] ex1 = { 11, 22, 33, 44, 55 };
+            Console.WriteLine($"Zadanie 1: {sumOfTable(ex1, 0)}");
 
+        }
+
+        public static int sumOfTable(int[] tab, int index)
+        {
+            int sum = 0;
+            if (index < tab.Length)
+            {
+                sum += tab[index++];
+                sum += sumOfTable(tab, index);
+            }
+            return sum;
         }
     }
 }
