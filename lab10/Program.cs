6f288bfd343cd20294f4dadd09a915918c33155a
lukasz.was <ljwasiu@gmail.com>
2022-05-17 10:14:02 +0200

    +lab10 skończone

--- lab10/Program.cs
+++ lab10/Program.cs
@@ -27,15 +27,14 @@ namespace lab10
             Preorder(node.Left, action);
             Preorder(node.Right, action);
         }
-
         public void InorderTraversal(Action<Node<T>> action)
         {
             Inorder(Root, action);
         }
-
         private void Inorder(Node<T> node, Action<Node<T>> action)
         {
             if (node == null) return;
+
 
             Inorder(node.Left, action);
             action.Invoke(node);
@@ -44,17 +43,59 @@ namespace lab10
 
         public void PostOrderTraversal(Action<Node<T>> action)
         {
-            Inorder(Root, action);
+            PostOrder(Root, action);
         }
 
-        private void Postorder(Node<T> node, Action<Node<T>> action)
+        private void PostOrder(Node<T> node, Action<Node<T>> action)
         {
             if (node == null) return;
 
-            Postorder(node.Left, action);
-            Postorder(node.Right, action);
+
+            PostOrder(node.Left, action);
+            PostOrder(node.Right, action);
             action.Invoke(node);
         }
+
+        public void LevelTraversal(Action<Node<T>> action)
+        {
+            Queue<Node<T>> q = new Queue<Node<T>>();
+            q.Enqueue(Root);
+            while (q.Count > 0)
+            {
+                Node<T> node = q.Dequeue();
+                action.Invoke(node);
+                if (node.Left != null) q.Enqueue(node.Left);
+                if (node.Right != null) q.Enqueue(node.Right);
+            }
+        }
+
+    }
+    class Expression : BinaryTree<string>
+    {
+        public double Evaluate()
+        {
+            return EvaluateNode(Root);
+        }
+
+        private double EvaluateNode(Node<string> node)
+        {
+            switch (node.Value)
+            {
+                case "+":
+                    return EvaluateNode(node.Left) + EvaluateNode(node.Right);
+                case "-":
+                    return EvaluateNode(node.Left) - EvaluateNode(node.Right);
+                case "*":
+                    return EvaluateNode(node.Left) * EvaluateNode(node.Right);
+                case "/":
+                    return EvaluateNode(node.Left) / EvaluateNode(node.Right);
+                case "^":
+                    return Math.Pow(EvaluateNode(node.Left), EvaluateNode(node.Right));
+                default:
+                    return double.Parse(node.Value);
+            }
+        }
+
     }
 
     class Program
@@ -63,16 +104,23 @@ namespace lab10
         {
             Node<string> node = new Node<string>() { Value = "+" };
             node.Left = new Node<string>() { Value = "*" };
-            node.Right = new Node<string>() { Value = "3" };
+            node.Right = new Node<string>() { Value = "^" };
             node.Left.Left = new Node<string>() { Value = "7" };
             node.Left.Right = new Node<string>() { Value = "9" };
+            node.Right.Left = new Node<string>() { Value = "2" };
+            node.Right.Right = new Node<string>() { Value = "4" };
+            Expression tree = new Expression() { Root = node };
 
-            BinaryTree<string> tree = new BinaryTree<string>() { Root = node };
-            tree.InorderTraversal((n) => Console.Write(n.Value));
+            tree.InorderTraversal(a => Console.Write(a.Value));
             Console.WriteLine();
-            tree.PostOrderTraversal((n) => Console.Write(n.Value));
+            tree.PostOrderTraversal(a => Console.Write(a.Value));
             Console.WriteLine();
-            tree.PreorderTraversal((n) => Console.Write(n.Value));
+            tree.PreorderTraversal(a => Console.Write(a.Value));
+            Console.WriteLine();
+            Console.WriteLine(tree.Evaluate());
+            Console.WriteLine();
+            tree.LevelTraversal(n => Console.WriteLine(n.Value));
+
         }
     }
 }
